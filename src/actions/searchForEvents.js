67e4c8ff866e8a376a2518a7cbe187463480f48f
Romina Torres <rominat@eventbrite.com>
2018-12-26 12:38:19 -0300

    Removing console.log

--- src/actions/searchForEvents.js
+++ src/actions/searchForEvents.js
@@ -1,12 +1,9 @@
 export const SEARCH = 'search';
 export const search = (term) => (
     (dispatch) => {
-        console.log(term);
-
         let data = require ('../data/eventsData');
         let events = data.data.events.filter((event) => event.name.includes(term.searchEvent));
 
-        console.log(events);
         dispatch({type: 'UPDATE_EVENTS', payload: events});
     }
 );
